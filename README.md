bed885702fc6b3c6ca63bf9f12cebc061d585764
georges <georgesdc13@gmail.com>
2025-02-11 12:30:29 -0300

    trabalhando com s3 como repositorio

--- README.md
+++ README.md
@@ -13,4 +13,5 @@ terraform apply -var 'instance_name=bia_dev_tf-2'
 terraform state list
 terraform state show aws_security_group.bia_dev
 terraform import aws_security_group.bia_dev sg-6a54sadf52165s0df
-terraform state rm aws_security_group.bia_dev sg-6a54sadf52165s0df 
+terraform state rm aws_security_group.bia_dev sg-6a54sadf52165s0df
+terraform init -migrate-state
--- main.tf
+++ main.tf
@@ -23,9 +23,9 @@ resource "aws_vpc_security_group_ingress_rule" "ingress_3001" {
   description       = "Porta 3001 liberada para o mundo"
   security_group_id = aws_security_group.bia_dev.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 3001
+  from_port         = 3002
   ip_protocol       = "tcp"
-  to_port           = 3001
+  to_port           = 3002
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
@@ -47,4 +47,28 @@ resource "aws_instance" "bia_dev" {
   root_block_device {
     volume_size = 10
   }
+}
+
+resource "aws_iam_policy" "iam_s3_policy" {
+  name        = "policy-terraform-tfstate"
+  description = "Policy to access s3 bucket"
+
+  policy = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:ListBucket",
+      "Resource": "arn:aws:s3:::bucket-bia-terraform"
+    },
+    {
+      "Effect": "Allow",
+      "Action": ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
+      "Resource": "arn:aws:s3:::bucket-bia-terraform/terraform.tfstate"
+    }
+  ]
+
+}
+EOT
 }
