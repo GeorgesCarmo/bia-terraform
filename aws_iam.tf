cccd16a2f0337bcfef1c95d234cc7ba44c444639
georges <georgesdc13@gmail.com>
2025-02-26 17:55:07 -0300

    corrigindo role-acesso-ssm

--- aws_iam.tf
+++ aws_iam.tf
@@ -32,18 +32,10 @@ resource "aws_iam_instance_profile" "role_acesso_ssm" {
 }
 
 resource "aws_iam_role" "role_acesso_ssm" {
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-  description           = "Allows EC2 instances to call AWS services on your behalf."
+  assume_role_policy    = "{\"Statement\":[{\"Action\":\"sts:AssumeRole\",\"Effect\":\"Allow\",\"Principal\":{\"Service\":\"ec2.amazonaws.com\"}}],\"Version\":\"2012-10-17\"}"
+  description           = null
   force_detach_policies = false
+  managed_policy_arns   = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess", "arn:aws:iam::aws:policy/AmazonECS_FullAccess", "arn:aws:iam::aws:policy/AmazonS3FullAccess", "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
   max_session_duration  = 3600
   name                  = "role-acesso-ssm"
   name_prefix           = null
--- aws_security_group.tf
+++ aws_security_group.tf
@@ -13,6 +13,15 @@ resource "aws_vpc_security_group_ingress_rule" "ingress_3001" {
   to_port           = 3001
 }
 
+resource "aws_vpc_security_group_ingress_rule" "ingress_ssh" {
+  description       = "Porta 22 liberada para o mundo"
+  security_group_id = aws_security_group.bia_dev.id
+  cidr_ipv4         = "131.255.229.116/32"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.bia_dev.id
   cidr_ipv4         = "0.0.0.0/0"
